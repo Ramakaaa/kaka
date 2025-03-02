7ce0aff7c2125893196684675621a1bd6426b4d9
Ramaka_ <15082006zaz@gmail.com>
2025-03-02 17:52:01 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("soundButton");
-    const audio = new Audio("sounds/бляяять.ogg");
-
-    button.addEventListener("click", () => {
-        audio.play();
-    });
-});
