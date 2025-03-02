0a5ed4989195e0ade5a04c4ba2463d0d46d8ed6e
Ramaka_ <15082006zaz@gmail.com>
2025-03-02 18:07:20 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("soundButton");
 
     button.addEventListener("click", () => {
-        let audio = new Audio("sounds/qwe.mp3").play();
+        let audio = new Audio("qwe.mp3").play();
         audio.play().catch(error => console.log("Ошибка воспроизведения:", error));
     });
 });
